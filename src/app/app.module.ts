f63ac9cea9eb9182e28ddbaad06dd4f362f60cf0
salma afify ali <fyfyslmy@gmail.com>
2021-08-04 23:48:05 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+const routes:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'contactUs',component:ContactUsComponent},
+  {path:'**',component:ErrorComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ContactUsComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
